559d968ff307300529351b677df7f73073af9307
EdgarPinto38 <edgarpinto010203@gmail.com>
2023-09-27 09:49:36 -0600

    corrigiendo js

--- JS/Practica5_Parcial2.js
+++ JS/Practica5_Parcial2.js
@@ -51,6 +51,6 @@ let elTriangulo = new Triangulo(5);
 let elCuadrado = new Cuadrado(3);
 let elPentagono = new Pentagono(3,3);
 
-console.log(`El área del cuadrado es: ${elCuadrado.area()} y su perímetro es: ${elCuadrado.perimetro()}`);
+console.log(`El área del cuadrado es: ${Cuadrado.area()} y su perímetro es: ${elCuadrado.perimetro()}`);
 console.log(`El área del triangulo es: ${elTriangulo.area()} y su perímetro es: ${elTriangulo.perimetro()}`);
 console.log(`El área del pentágono es: ${elPentagono.area()} y su perímetro es: ${elPentagono.perimetro()}`);
