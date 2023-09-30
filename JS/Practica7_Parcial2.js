ac2d1c31959daf13d3c397e93f198e08ce98cf7e
EdgarPinto38 <edgarpinto010203@gmail.com>
2023-09-30 14:40:30 -0600

    Agregando practica 7

--- JS/Practica7_Parcial2.js
+++ JS/Practica7_Parcial2.js
@@ -1,10 +1,15 @@
 function factorial(){
+
     n=prompt('Ingrese un numero:','');
+
     parseInt(n, 10)
-    //et num=9;
+    
     let resultado=1;
+    
     for(let i = n;i>=1;i--){
+
         resultado *= i;
+        
     }
 
     return resultado;
