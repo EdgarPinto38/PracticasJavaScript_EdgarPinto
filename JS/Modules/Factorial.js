509ded8dd54dec3dad8cc2b0951fa6887e73d182
EdgarPinto38 <edgarpinto010203@gmail.com>
2023-10-04 00:57:01 -0600

    no se suben los cambios jajaja

--- JS/Modules/Factorial.js
+++ JS/Modules/Factorial.js
@@ -1,14 +1,13 @@
 export function Factorial()
 {
-    let 
-    n = prompt
-    ('Ingrese un numero:','');
+    let n = prompt ('Ingrese un numero:','');
 
     parseInt(n, 10)
     
     let resultado=1;
     
-    for(let i = n;i>=1;i--){
+    for(let i = n;i>=1;i--)
+    {
 
         resultado *= i;
         
